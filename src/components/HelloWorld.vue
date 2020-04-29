522ec23bec67c427b8bfd1456b573e27904d11bf
Rose Le <rose.le.coder@gmail.com>
2020-04-29 18:04:53 +1200

    Initial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,153 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-card class="mx-auto"
+    max-width="500">
+      <v-card-title class="text-center teal">
+         Todos List
+      </v-card-title>
+      <v-card-text>
+<v-btn  class="ma-2" outlined color="black" @click="save">Add New</v-btn>
+  <v-text-field  
+     
+      class="new-todo"
+      autofocus
+      autocomplete="off"
+      v-model="defaultItem"
+      @keyup.enter="save"
+      placeholder="What needs to be done?">
+      </v-text-field>
+   <AddTodo v-on:add-todo="addTodo" />
+ <Todos v-bind:todos="todos" />
+
+      </v-card-text>
+     <v-card
+    class="mx-auto"
+    max-width="500"
+  >
+    <v-list shaped>
+      <v-list-item-group
+        v-model="model"
+        multiple
+      >
+        <template v-for="(item, i) in items">
+          <v-divider
+            v-if="!item"
+            :key="`divider-${i}`"
+          ></v-divider>
+
+          <v-list-item
+            v-else
+            :key="`item-${i}`"
+            :value="item"
+            active-class="cyan--text text--accent-4"
+          >
+            <template v-slot:default="{ active, toggle }">
+              <v-list-item-content>
+                <v-list-item-title v-text="item"></v-list-item-title>
+              </v-list-item-content>
+
+              <v-list-item-action>
+                <v-checkbox
+                  :input-value="active"
+                  :true-value="item"
+                  color="cyan accent-4"
+                  @click="toggle"
+                ></v-checkbox>
+      
+             
+              </v-list-item-action>
+            </template>
+          </v-list-item>
+        </template>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+ import axios from 'axios';
+ import Todos from '../components/Todos';
+import AddTodo from '../components/AddTodo';
+  export default {
+    name: 'HelloWorld',
+     components: {
+    Todos,
+    AddTodo
+  },
+
+    data: () => ({
+      todos: [],
+      items: [
+        'Survey users',
+        'Design a prototype',
+        'Coding',
+        'Reading a book',
+        'Go to supermarket',
+        'Check emails',
+      ],
+      defaultItem:"",
+      model:"Coding",
+
+    }),
+   methods:{
+     created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err));
   }
-}
+   },
+   addTodo(newTodo) {
+      const { title, completed } = newTodo;
+
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch(err => console.log(err));
+    },
+     save () {
+        if (this.items > -1) {
+          Object.assign(this.items,this.newTodo)
+        } else {
+          this.items.push(this.defaultItem)
+        }
+        this.close()
+      },
+    // deleteTodo(id) {
+    //   axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    //     .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+    //     .catch(err => console.log(err));
+    // },
+  }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
+  }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+<div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete" v-bind:checked="todo.completed">
+      {{todo.title}}
+      <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+      </p>
+     
+  </div>
+  </div>
+</template>
+
+<script>
+  export default {
+   name: "TodoItem",
+  props: ["todo"],
+   data: () => ({
+      todos: [],
+      items: [
+        'Survey users',
+        'Design a prototype',
+        'Coding',
+        'Reading a book',
+        'Go to supermarket',
+        'Check emails',
+      ],
+      model:"Coding",
+
+    }),
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    }
+  }
+  }
+</script>
+
+<style  scoped>
+ .todo-item {
+    background: #f4f4f4;
+    padding: 30px;
+    border-bottom: 1px #ccc dotted;
+
+  }
+
+  .is-complete {
+    text-decoration: line-through;
+  }
+</style>
